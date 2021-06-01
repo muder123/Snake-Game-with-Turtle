72d714ebffb365c37c4d1900b9e7c6f6f7fab5b2
muder123 <wycgjmh@gmail.com>
2021-06-01 20:56:17 +1000

    high score record functionality added

--- main.py
+++ main.py
@@ -34,13 +34,12 @@ while game_is_on:
 
     # Detect collison with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
         
     # Detect collison with taile
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+            scoreboard.reset()
+            snake.reset()
 screen.exitonclick()
